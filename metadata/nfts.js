56d2e7c731d97c8ed8b7e78afd8aa0d55eeca163
VinayKumar2004 <94241385+VinayKumar2004@users.noreply.github.com>
2023-07-24 19:58:18 +0530

    Add files via upload

---
+++ metadata/nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+  {
+    name: "0",
+    description: "A cricketer wearing blue jersey cheering the crowd",
+    image: "QmUWBke3J2wvvBJrJBb2BLTts3mdRGhBV3ogSd1qhScEE5",
+  },
+  {
+    name: "1",
+    description: "A cricketer wearing blue jersey cheering the crowd",
+    image: "QmaVeAWwE4Au6GsfHa97SMEKowCLSgWRFF2wRxfHbntTMr",
+  }, //change
+  {
+    name: "2",
+    description: "A cricketer wearing blue jersey cheering the crowd",
+    image: "QmRYsPfPCX8QeJXeo19wDXCPpt3pwFLjh64J5TUwEJWJKw",
+  },
+  {
+    name: "3",
+    description: "A cricketer wearing blue jersey cheering the crowd",
+    image: "QmXzD5cT1zGTGexsZd3xG8ZjVmkDBNFG8NhPHstPsYLFNP",
+  },
+  {
+    name: "4",
+    description: "A cricketer wearing blue jersey cheering the crowd.",
+    image: "QmQzyHFv69WRmnrwvqvSYDZfHjndxJTSLWmQo8qjCXpPbN",
+  },
+];
+
+module.exports = nfts;
